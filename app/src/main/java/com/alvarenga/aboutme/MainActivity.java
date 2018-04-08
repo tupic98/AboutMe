c0e54d734aef74ce490bfb0e9e67d2f6e0937f33
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-04-08 01:07:02 -0600

    Changed Uriparse to Uri bitmap for sharing image support

--- app/src/main/java/com/alvarenga/aboutme/MainActivity.java
+++ app/src/main/java/com/alvarenga/aboutme/MainActivity.java
@@ -1,8 +1,12 @@
 package com.alvarenga.aboutme;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,11 +16,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,25 +48,55 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.sharebutton) {
-            String info = "Nombre: " + getResources().getText(R.string.name) + "\n" +
+        String info = "Nombre: " + getResources().getText(R.string.name) + "\n" +
                 "Carrera: " + getResources().getText(R.string.career_name) + "\n" +
                 "Github: " + getResources().getText(R.string.github_name) + "\n" +
                 "Correo: " + getResources().getText(R.string.email) + "\n" +
                 "Twitter: " + getResources().getText(R.string.tw_name) + "\n" +
                 "Telefono : " + getResources().getText(R.string.phone) + "\n";
-            Intent sendIntent = new Intent();
-            sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, ("About me \n" + info) );
-            final File photoFile = new File(getFilesDir(), "aboutmee.jpg");
-            sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(photoFile));
-            sendIntent.setType("*/*");
-            sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            startActivity(Intent.createChooser(sendIntent,getResources().getText(R.string.send)));
+        final ImageView img = (ImageView) findViewById(R.id.photo);
+        Uri bmpUri = getLocalBitmapUri(img);
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.sharebutton) {
+            if (bmpUri != null) {
+                // Construct a ShareIntent with link to image
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, ("About me: \n" + info));
+                shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+                shareIntent.setType("image/*");
+                // Launch sharing dialog for image
+                startActivity(Intent.createChooser(shareIntent, "Share AboutMe"));
+            } else {
+                // ...sharing failed, handle error
+            }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_aboutme_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 }
